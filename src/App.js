eb60c35964521338a29c6a96a3e8d25ef9d4747e
3-Hz <edward.h.zhao@gmail.com>
2021-03-22 21:21:10 -0500

    displaying ACLED data, experimental flyto feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+import React from 'react';
 import Map from './components/Map.js';
+import axios from 'axios';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Map />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mapRef: null,
+      conflicts: []
+    };
+    this.handleMapRef = this.handleMapRef.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:1337/events')
+      .then(data => {
+        let conflicts = data.data.data;
+        this.setState({
+          conflicts: conflicts
+        })
+      })
+  }
+
+  handleMapRef(mapRef) {
+    this.setState({
+      mapRef: mapRef
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Map conflicts={this.state.conflicts} handleMapRef={this.handleMapRef} mapRef={this.state.mapRef}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,49 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Popup, Circle, useMapEvents } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-const Map = () => {
+const Map = ({ conflicts, handleMapRef, mapRef }) => {
+
+  // function handleOnFlyTo(coords) {
+  //   const map = mapRef;
+  //   map.flyTo(coords, 14, { duration: 2 });
+  // }
+
   return (
-    <MapContainer center={[0, 0]} zoom={2} scrollWheelZoom={true}>
+    <MapContainer center={[0, 0]} zoom={2} scrollWheelZoom={true} whenCreated={map => handleMapRef(map)}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {conflicts.map(conflict => (
+          <Circle
+            key={conflict.data_id}
+            center={[Number(conflict.latitude),Number(conflict.longitude)]}
+            color={'red'}
+            weight={7}
+            fillColor={'#f03'}
+            radius={200}
+            // eventHandlers={{
+            //   click: (e) => {
+            //     handleOnFlyTo([Number(conflict.latitude),Number(conflict.longitude)]);
+            //   }
+            // }}
+          >
+            <Popup>
+              Event: {conflict.event_type}
+              <br></br>
+              Date: {conflict.event_date}
+              <br></br>
+              Type: {conflict.sub_event_type}
+              <br></br>
+              Actors: {conflict.actor1} - {conflict.actor2}
+              <br></br>
+              Fatalities: {conflict.fatalities}
+              <br></br>
+              Notes: {conflict.notes}
+            </Popup>
+          </Circle>
+        ))}
+
     </MapContainer>
   )
 }
